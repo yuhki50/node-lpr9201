130c049868618254a92c637d96afb28cce7dfe2a
yuhki50 <yuhki50@gmail.com>
2014-08-23 10:54:22 +0900

    不要なファイルを削除

--- lib/IReceiver.java
+++
@@ -1,8 +0,0 @@
-package com.teamlab.communication.wireless.lpr9201;
-
-/**
- * データ受信のインターフェイス
- */
-public interface IReceiver {
-	Integer[] read();
-}
--- lib/ISender.java
+++
@@ -1,8 +0,0 @@
-package com.teamlab.communication.wireless.lpr9201;
-
-/**
- * データ送信のインターフェイス
- */
-public interface ISender {
-	void write(Integer[] data);
-}
