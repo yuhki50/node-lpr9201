108766d6c5ddbb66d5eb96856c5bc9d8b1beb356
yuhki50 <yuhki50@gmail.com>
2015-08-14 03:29:06 +0900

    gulpタスクを追加

---
+++ gulp/tasks/mocha.ts
@@ -0,0 +1,8 @@
+/// <reference path="../../typings/tsd.d.ts"/>
+
+var gulp = require('gulp');
+var ts = require('gulp-mocha');
+
+gulp.task('mocha:compile:develop', () => {
+
+});
---
+++ gulp/tasks/typescript.ts
@@ -0,0 +1,8 @@
+/// <reference path="../../typings/tsd.d.ts"/>
+
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+
+gulp.task('typescript:compile:develop', () => {
+
+});
