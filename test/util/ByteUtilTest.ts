849d3afa0c5b072456f722b91595338c89fdaa47
yuhki50 <yuhki50@gmail.com>
2015-08-14 03:29:43 +0900

    テストを修正

--- test/util/ByteUtilTest.ts
+++ test/util/ByteUtilTest.ts
@@ -1,137 +1,149 @@
-class ByteUtilTest {
-    public void before(){
+/// <reference path="../../typings/tsd.d.ts" />
+/// <reference path="../../lib/util/ByteUtil.ts" />
 
-    }
+import chai = require('chai');
+var expect = chai.expect;
+var lpr9201 = require('../lib');
 
-    public void after(){
+//class ByteUtilTest {
+    //public void before(){
+    //
+    //}
 
-    }
+    //public void after(){
+    //
+    //}
 
     /**
      * ビックエンディアンで1byte分の入力で1byte分取り出す
      * Method: splitBitEndian(long value, int length)
      */
-    public void testSplitBigEndianIn1ByteOut1Byte() {
-        Assert.assertArrayEquals(new Integer[]{0x01}, ByteUtil.splitBigEndian(0x01L, 1));
-    }
+        //public void testSplitBigEndianIn1ByteOut1Byte() {
+        //    Assert.assertArrayEquals(new Integer[]{0x01}, ByteUtil.splitBigEndian(0x01L, 1));
+        //}
+    describe('splitBigEndianIn1ByteOut1Byte', () => {
+        //expect([0x01], lpr9201.util.ByteUtil.splitBigEndian(0x01, 1));
+        //console.log(ByteUtil.splitBigEndian(0x01, 1));
+        //console.log(ByteUtil);
+    });
 
     /**
      * ビックエンディアンで2byte分の入力で1byte分取り出す
      * Method: splitBitEndian(long value, int length)
      */
-    public void testSplitBigEndianIn2ByteOut1Byte() {
-        Assert.assertArrayEquals(new Integer[]{0x00}, ByteUtil.splitBigEndian(0x0100L, 1));
-    }
+    //public void testSplitBigEndianIn2ByteOut1Byte() {
+    //    Assert.assertArrayEquals(new Integer[]{0x00}, ByteUtil.splitBigEndian(0x0100L, 1));
+    //}
 
     /**
      * ビックエンディアンで2byte分の入力で2byte分取り出す
      * Method: splitBitEndian(long value, int length)
      */
-    public void testSplitBigEndianIn2ByteOut2Byte() {
-        Assert.assertArrayEquals(new Integer[]{0x01, 0x02}, ByteUtil.splitBigEndian(0x0102L, 2));
-    }
+    //public void testSplitBigEndianIn2ByteOut2Byte() {
+    //    Assert.assertArrayEquals(new Integer[]{0x01, 0x02}, ByteUtil.splitBigEndian(0x0102L, 2));
+    //}
 
     /**
      * ビックエンディアンで3byte分の入力で2byte分取り出す
      * Method: splitBitEndian(long value, int length)
      */
-    public void testSplitBigEndianIn3ByteOut2Byte() {
-        Assert.assertArrayEquals(new Integer[]{0x02, 0x03}, ByteUtil.splitBigEndian(0x010203L, 2));
-    }
+    //public void testSplitBigEndianIn3ByteOut2Byte() {
+    //    Assert.assertArrayEquals(new Integer[]{0x02, 0x03}, ByteUtil.splitBigEndian(0x010203L, 2));
+    //}
 
     /**
      * ビックエンディアンで3byte分の入力で3byte分取り出す
      * Method: splitBitEndian(long value, int length)
      */
-    public void testSplitBigEndianIn3ByteOut3Byte() {
-        Assert.assertArrayEquals(new Integer[]{0x01, 0x02, 0x03}, ByteUtil.splitBigEndian(0x010203L, 3));
-    }
+    //public void testSplitBigEndianIn3ByteOut3Byte() {
+    //    Assert.assertArrayEquals(new Integer[]{0x01, 0x02, 0x03}, ByteUtil.splitBigEndian(0x010203L, 3));
+    //}
 
     /**
      * リトルエンディアンで1byte分の入力で1byte分取り出す
      * Method: splitLittleEndian(long value, int length)
      */
-    public void testSplitLittleEndianIn1ByteOut1Byte() {
-        Assert.assertArrayEquals(new Integer[]{0x01}, ByteUtil.splitLittleEndian(0x01L, 1));
-    }
+    //public void testSplitLittleEndianIn1ByteOut1Byte() {
+    //    Assert.assertArrayEquals(new Integer[]{0x01}, ByteUtil.splitLittleEndian(0x01L, 1));
+    //}
 
     /**
      * リトルエンディアンで2byte分の入力で1byte分取り出す
      * Method: splitLittleEndian(long value, int length)
      */
-    public void testSplitLittleEndianIn2ByteOut1Byte() {
-        Assert.assertArrayEquals(new Integer[]{0x00}, ByteUtil.splitLittleEndian(0x0100L, 1));
-    }
+    //public void testSplitLittleEndianIn2ByteOut1Byte() {
+    //    Assert.assertArrayEquals(new Integer[]{0x00}, ByteUtil.splitLittleEndian(0x0100L, 1));
+    //}
 
     /**
      * リトルエンディアンで2byte分の入力で2byte分取り出す
      * Method: splitLittleEndian(long value, int length)
      */
-    public void testSplitLittleEndianIn2ByteOut2Byte() {
-        Assert.assertArrayEquals(new Integer[]{0x02, 0x01}, ByteUtil.splitLittleEndian(0x0102L, 2));
-    }
+    //public void testSplitLittleEndianIn2ByteOut2Byte() {
+    //    Assert.assertArrayEquals(new Integer[]{0x02, 0x01}, ByteUtil.splitLittleEndian(0x0102L, 2));
+    //}
 
     /**
      * リトルエンディアンで3byte分の入力で2byte分取り出す
      * Method: splitLittleEndian(long value, int length)
      */
-    public void testSplitLittleEndianIn3ByteOut2Byte() {
-        Assert.assertArrayEquals(new Integer[]{0x03, 0x02}, ByteUtil.splitLittleEndian(0x010203L, 2));
-    }
+    //public void testSplitLittleEndianIn3ByteOut2Byte() {
+    //    Assert.assertArrayEquals(new Integer[]{0x03, 0x02}, ByteUtil.splitLittleEndian(0x010203L, 2));
+    //}
 
     /**
      * リトルエンディアンで3byte分の入力で3byte分取り出す
      * Method: splitLittleEndian(long value, int length)
      */
-    public void testSplitLittleEndianIn3ByteOut3Byte() {
-        Assert.assertArrayEquals(new Integer[]{0x03, 0x02, 0x01}, ByteUtil.splitLittleEndian(0x010203L, 3));
-    }
+    //public void testSplitLittleEndianIn3ByteOut3Byte() {
+    //    Assert.assertArrayEquals(new Integer[]{0x03, 0x02, 0x01}, ByteUtil.splitLittleEndian(0x010203L, 3));
+    //}
 
     /**
      * ビックエンディアンで1byte分の配列を結合する
      * Method: mergeBigEndian(int[] values)
      */
-    public void testMergeBigEndian1Byte() {
-        Assert.assertEquals(0x01L, ByteUtil.mergeBigEndian(new int[]{0x01}));
-    }
+    //public void testMergeBigEndian1Byte() {
+    //    Assert.assertEquals(0x01L, ByteUtil.mergeBigEndian(new int[]{0x01}));
+    //}
 
     /**
      * ビックエンディアンで2byte分の配列を結合する
      * Method: mergeBigEndian(int[] values)
      */
-    public void testMergeBigEndian2Byte() {
-        Assert.assertEquals(0x0102L, ByteUtil.mergeBigEndian(new int[]{0x01, 0x02}));
-    }
+    //public void testMergeBigEndian2Byte() {
+    //    Assert.assertEquals(0x0102L, ByteUtil.mergeBigEndian(new int[]{0x01, 0x02}));
+    //}
 
     /**
      * ビックエンディアンで3byte分の配列を結合する
      * Method: mergeBigEndian(int[] values)
      */
-    public void testMergeBigEndian3Byte() {
-        Assert.assertEquals(0x010203L, ByteUtil.mergeBigEndian(new int[]{0x01, 0x02, 0x03}));
-    }
+    //public void testMergeBigEndian3Byte() {
+    //    Assert.assertEquals(0x010203L, ByteUtil.mergeBigEndian(new int[]{0x01, 0x02, 0x03}));
+    //}
 
     /**
      * リトルエンディアンで1byte分の配列を結合する
      * Method: mergeLittleEndian(int[] values)
      */
-    public void testMergeLittleEndian1Byte() {
-        Assert.assertEquals(0x01L, ByteUtil.mergeLittleEndian(new int[]{0x01}));
-    }
+    //public void testMergeLittleEndian1Byte() {
+    //    Assert.assertEquals(0x01L, ByteUtil.mergeLittleEndian(new int[]{0x01}));
+    //}
 
     /**
      * リトルエンディアンで2byte分の配列を結合する
      * Method: mergeLittleEndian(int[] values)
      */
-    public void testMergeLittleEndian2Byte() {
-        Assert.assertEquals(0x0201L, ByteUtil.mergeLittleEndian(new int[]{0x01, 0x02}));
-    }
+    //public void testMergeLittleEndian2Byte() {
+    //    Assert.assertEquals(0x0201L, ByteUtil.mergeLittleEndian(new int[]{0x01, 0x02}));
+    //}
 
     /**
      * リトルエンディアンで3byte分の配列を結合する
      * Method: mergeLittleEndian(int[] values)
      */
-    public void testMergeLittleEndian3Byte() {
-        Assert.assertEquals(0x030201L, ByteUtil.mergeLittleEndian(new int[]{0x01, 0x02, 0x03}));
-    }
-}
+    //public void testMergeLittleEndian3Byte() {
+    //    Assert.assertEquals(0x030201L, ByteUtil.mergeLittleEndian(new int[]{0x01, 0x02, 0x03}));
+    //}
+//}
