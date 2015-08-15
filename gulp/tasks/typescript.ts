9303f41736a848da0465f99128dd22d4683f2325
yuhki50 <yuhki50@gmail.com>
2015-08-16 00:59:39 +0900

    typescriptをビルドするスクリプトをgulpタスクを追加

--- gulp/tasks/typescript.ts
+++ gulp/tasks/typescript.ts
@@ -1,8 +1,28 @@
 /// <reference path="../../typings/tsd.d.ts"/>
 
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var ts = require('gulp-typescript');
+var merge = require('merge2');
 
-gulp.task('typescript:compile:develop', () => {
-
+var tsProject = ts.createProject({
+  declarationFiles: true,
+  noExternalResolve: true
 });
+
+gulp.task('typescript:build', () => {
+  var tsResult = gulp.src('src/**/*.ts')
+    .pipe(ts(tsProject));
+
+  return merge([
+    tsResult.dts.pipe(concat('lpr9201.d.ts')),  // 定義ファイル
+    tsResult.js.pipe(concat('lpr9201.js')),  // JavaScriptファイル
+  ].map((value, index, arr) => {
+    return value.pipe(gulp.dest('lib'))
+  }));
+});
+
+gulp.task('typescript:build:watch', ['typescript:compile'], () => {
+  gulp.watch('src/**/*.ts', ['scripts']);
+});
+
